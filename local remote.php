ec07c07e60f60ad1ddc9d934e6326f8245202860
Kosta The Russian Kondratenko <kosta@headstudios.com.au>
2019-09-09 12:46:13 +1000

    Editing local remote file to check push to GitHub and then pull from cPanel to see if it overwrites the wp-config file

--- local remote.php
+++ local remote.php
@@ -1,1 +1,4 @@
-<?php echo "This is a file created locally on my Mac machine - to be pushed to the GitHub server"; ?>
+<?php echo "This is a file created locally on my Mac machine - to be pushed to the GitHub server";
+
+echo "This is an update to check if code files will be over written";
+?>
