cb142f1609149ff8b4c7fa700fc5b4a90fd5b267
Kosta The Russian Kondratenko <kosta@headstudios.com.au>
2019-09-09 12:33:43 +1000

    Pull file edited

--- pull_file.php
+++ pull_file.php
@@ -1,1 +1,4 @@
-<?php echo "This is the file that should be pulled to your local computer"; ?>
+<?php echo "This is the file that should be pulled to your local computer";
+
+echo "This is a pull file that is being edited";
+?>
